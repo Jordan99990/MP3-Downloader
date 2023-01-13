dc94362ff2141d2cd281eea891fe536c24141591
Alex Ardelean <alex.ardelean606@gmail.com>
2023-01-13 10:14:06 +0200

    interface

---
+++ constants.py
@@ -0,0 +1,4 @@
+TITLE = "MP3 Downloader for YouTube"
+AUTHORS = ["Norbyke", "dENJI"]
+
+WINDOW_SIZE = (800, 325)
--- download.py
+++ download.py
@@ -1,33 +1,52 @@
 from pytube import YouTube
 import os
 
+download_history = []
+
 def check_url(url: str) -> str:
+    if not '.com' in url:
+      return 'invalid'
     if 'youtube' in  url[:url.index('.com')]:
-        return 'youtube'
-    elif 'soundcloud' in url[:url.index('.com')]:
-        return 'soundcloud'
+      return 'youtube'
+    if 'soundcloud' in url[:url.index('.com')]:
+      return 'soundcloud'
     
     return 'neither'
 
-def get_audio_file() -> None:
-    url = input('Enter url = ')
+def download_yt(url, save_path) -> None:
+  print(save_path)
+  url = YouTube(url)
+  audio = url.streams.filter(only_audio = True).first()
+      
+  output = audio.download(output_path = save_path)
+      
+  base, ext = os.path.splitext(output)
+  new_file = base + '.mp3'
+  os.rename(output, new_file)
+      
+  print(url.title, 'was downloaded')
+
+
+# def get_audio_file() -> None:
+#     url = input('Enter url = ')
     
-    if 'youtube' not in url[:url.index('.com')]:
-        return
+#     if 'youtube' not in url[:url.index('.com')]:
+#       return
     
-    url = YouTube(url)
+#     url = YouTube(url)
         
-    audio = url.streams.filter(only_audio = True).first()
+#     audio = url.streams.filter(only_audio = True).first()
         
-    path = input('Enter download path = ')
+#     path = input('Enter download path = ')
         
-    output = audio.download(output_path = path)
+#     output = audio.download(output_path = path)
         
-    base, ext = os.path.splitext(output)
-    new_file = base + '.mp3'
-    os.rename(output, new_file)
+#     base, ext = os.path.splitext(output)
+#     new_file = base + '.mp3'
+#     os.rename(output, new_file)
         
-    print(url.title, ' a fost descarcata')
+#     print(url.title, 'was downloaded')
+#     download_history.append(url.title)
         
-if __name__ == '__main__':
-    get_audio_file()
+# if __name__ == '__main__':
+#     get_audio_file()
